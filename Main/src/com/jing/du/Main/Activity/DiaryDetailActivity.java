95b645744ece283aa108cacbaf88b8c080139536
chenyongqiang <chen.yongqiang@bestv.com.cn>
2015-07-06 14:13:07 +0800

    add my title bar

--- Main/src/com/jing/du/Main/Activity/DiaryDetailActivity.java
+++ Main/src/com/jing/du/Main/Activity/DiaryDetailActivity.java
@@ -6,11 +6,13 @@ import android.os.Handler;
 import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import com.beardedhen.androidbootstrap.FontAwesomeText;
 import com.jing.du.Main.Adapter.AddDiaryItemAdapter;
 import com.jing.du.Main.Model.Diary;
 import com.jing.du.Main.Model.DiaryItem;
@@ -31,6 +33,8 @@ public class DiaryDetailActivity extends BaseActivity {
     @InjectView(R.id.lv_diary_item)
     ListView lvDiaryItem;
 
+    FontAwesomeText tvMenu;
+
     private int diaryId;
     private Diary diary;
     private List<DiaryItem> diaryItemList = new ArrayList<DiaryItem>();
@@ -50,25 +54,29 @@ public class DiaryDetailActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.layout_diary_detail);
         ButterKnife.inject(this);
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         diaryId = bundle.getInt("diary_id", 0);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.my_title);
         afterView();
     }
 
 
     private void afterView() {
+        tvMenu = (FontAwesomeText)findViewById(R.id.tv_menu);
+        tvMenu.setOnClickListener(menuClick);
         initData();
     }
 
     private void initView() {
         LayoutInflater inflater = getLayoutInflater();
         View headView = inflater.inflate(R.layout.layout_diary_detail_header, null);
-        ((TextView)headView.findViewById(R.id.tv_create_time)).setText(DateUtils.getStringOfDate(diary.getCreateTime()));
-        ((TextView)headView.findViewById(R.id.tv_create_address)).setText(DateUtils.getStringOfDate(diary.getCreateTime()));
-        ((ImageView)headView.findViewById(R.id.iv_create_weather)).setImageResource(CommonConstant.WEATHER_RESOUCE[diary.getWeatherType()]);
+        ((TextView) headView.findViewById(R.id.tv_create_time)).setText(DateUtils.getStringOfDate(diary.getCreateTime()));
+        ((TextView) headView.findViewById(R.id.tv_create_address)).setText(DateUtils.getStringOfDate(diary.getCreateTime()));
+        ((ImageView) headView.findViewById(R.id.iv_create_weather)).setImageResource(CommonConstant.WEATHER_RESOUCE[diary.getWeatherType()]);
         lvDiaryItem.addHeaderView(headView);
         AddDiaryItemViewHolder addDiaryItemViewHolder = new AddDiaryItemViewHolder();
         AddDiaryItemAdapter addDiaryItemAdapter = new AddDiaryItemAdapter(this, diaryItemList, addDiaryItemViewHolder, R.layout.detail_diary_item);
@@ -93,4 +101,11 @@ public class DiaryDetailActivity extends BaseActivity {
         super.initEvent();
     }
 
+    
+    View.OnClickListener menuClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
 }
